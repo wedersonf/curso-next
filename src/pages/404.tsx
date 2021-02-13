5ba5731d946567e5c2847dc4795847df9c8135ce
wedersonf <wedymf@hotmail.com>
2021-02-12 21:23:13 -0300

    pagina 404 customizada

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { Title } from "../styles/pages/Home";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Title>Page not found</Title>
+
+      <p>are you lost?</p>
+    </div>
+  )
+}
